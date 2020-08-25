304b20945337b38d7c5e6b381a798d526e479c99
syco1316 <syco1316@gmail.com>
2020-08-24 23:50:54 -0500

    Installed Podfiles and set up the view controllers and storyboard

---
+++ InstagramCloneApp/InstagramCloneApp/Controllers/CoreTabs/ExploreViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ExploreViewController.swift
+//  InstagramCloneApp
+//
+//  Created by Jonathan Shoemaker on 8/24/20.
+//  Copyright © 2020 Jonathan Shoemaker. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstagramCloneApp/InstagramCloneApp/Controllers/CoreTabs/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramCloneApp
+//
+//  Created by Jonathan Shoemaker on 8/24/20.
+//  Copyright © 2020 Jonathan Shoemaker. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
