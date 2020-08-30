a8d79d2b69cd93be950d1cb882f55996a4335e8d
syco1316 <syco1316@gmail.com>
2020-08-30 11:42:56 -0500

    Set up login view controller( buttons, methods, links, constraints)

---
+++ InstagramCloneApp/InstagramCloneApp/Resources/Extensions.swift
@@ -0,0 +1,37 @@
+//
+//  Extensions.swift
+//  InstagramCloneApp
+//
+//  Created by Jonathan Shoemaker on 8/30/20.
+//  Copyright © 2020 Jonathan Shoemaker. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    
+    public var width: CGFloat {
+        return frame.size.width
+    }
+    
+    public var height: CGFloat {
+        return frame.size.height
+    }
+    
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+    
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+}
