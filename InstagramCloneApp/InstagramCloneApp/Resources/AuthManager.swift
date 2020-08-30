be1e7f0685dc4db6b1482f8f379955fade372187
syco1316 <syco1316@gmail.com>
2020-08-29 21:50:18 -0500

    setup subviews and home view controller functions

--- InstagramCloneApp/InstagramCloneApp/Resources/AuthManager.swift
+++ InstagramCloneApp/InstagramCloneApp/Resources/AuthManager.swift
@@ -7,7 +7,7 @@
 //
 
 import Foundation
-import FirebaseDatabase
+import FirebaseAuth
 
 public class AuthManager {
 
--- InstagramCloneApp/InstagramCloneApp/Resources/StorageManager.swift
+++ InstagramCloneApp/InstagramCloneApp/Resources/StorageManager.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+import FirebaseStorage
+
+public class StorageManager {
+    
+    static let shared = StorageManager()
+    
+    //MARK: Public
+}
